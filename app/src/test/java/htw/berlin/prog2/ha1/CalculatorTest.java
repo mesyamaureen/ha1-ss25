e1ec1ae1b0285e7f707866b4f195d75387508598
mesyamaureen <mesyamaureen@gmail.com>
2025-04-19 21:58:36 +0200

    2 Teilaufgabe

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -104,5 +104,49 @@ class CalculatorTest {
 
         assertEquals(expected, actual);
     }
+
+    // Test: Check = behaviour when pressed multiple times after performing an initial calculation
+    @Test
+    @DisplayName("should repeat last operation when pressing equals multiple times")
+    void testMultipleEqualsPress() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(5);
+        calc.pressBinaryOperationKey("+");
+        calc.pressDigitKey(8);
+        calc.pressEqualsKey();
+        calc.pressEqualsKey();
+
+        String expected = "21";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("should reset screen to zero after pressing clear key once")
+    void testClearAfterOperation() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(5);
+        calc.pressBinaryOperationKey("+");
+        calc.pressDigitKey(3);
+        calc.pressEqualsKey();
+        calc.pressClearKey();
+
+        String expected = "0";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+
+        calc.pressBinaryOperationKey("+");
+        calc.pressDigitKey(2);
+        calc.pressEqualsKey();
+
+        expected = "10";
+        actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
 }
 
